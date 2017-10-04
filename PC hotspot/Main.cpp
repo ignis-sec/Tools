6172facb6dff341d70e734cf96006a78eccf633d
Ata Hakcil <ahakcil@gmail.com>
2017-10-04 21:33:14 +0300

    Menu

--- PC hotspot/Main.cpp
+++ PC hotspot/Main.cpp
@@ -1,11 +1,17 @@
+//Ata Hakçýl
 #include <string>																	//includes
 #include <iostream>
 #include <fstream>
 
+//////////////////////////////////////////////////////////////////////////////////////
 
+void draw_Menu();																	//function templates
+
+
+//////////////////////////////////////////////////////////////////////////////////////
 
 using namespace std;																//namespace
-
+//////////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char* argv)
 {
 	string line;																	//variables
@@ -26,21 +32,64 @@ int main(int argc, char* argv)
 	{
 		logfile << "Cannot open driver.txt, shutting down.";
 		cout << "Cannot open driver.txt, shutting down.";
-		return 0;
+		return -1;
 	}
-	
+	int linecount = 0;
 	while (getline(driverDetails,line))												//search for  "Hosted network supported  : Yes"
-	{
-		if (line.compare("    Hosted network supported  : Yes") == 0) cout << "Mobile Hotspot is supported. Continuing with mobile hotspot.\n";
-		if (line.find("Driver",6)==1)
-		{ 
-			line.substr(28); 
-			//cardName.append(line); 
+	{																				//also check the wifi cards name
+		if (line.compare("    Hosted network supported  : Yes") == 0)
+		{
+			cout << "Mobile Hotspot is supported. Continuing with mobile hotspot.\n";
+			bCanStart = true;
 		}
-		bool bCanStart = true;
+
+		linecount++;
+		if (linecount == 4)
+		{
+			cardName.append(line);
+		}
+		
 	}
-	cout << "Wireless card name:" << cardName <<'\n';
+	if (!bCanStart)																	//if cant start
+	{
+		cout << "Wireless card name:" << cardName << '\n';
+		cout << "Wireless card is flagged as incompatible for hosted networks. Try Updating the drivers.\n";
+		return -2;
+	}
+
+	draw_Menu();
 
 	return 0;
 }
 
+//////////////////////////////////////////////////////////////////////////////////////
+void draw_Menu()
+{	
+	fstream passwordfile;															//open file to read/write last used password and id to remember
+	passwordfile.open(SSID_Password.txt);
+	cout << "\n\n\n";
+	cout << "                                                             \n";
+	cout << "                                                             \n";
+	cout << "                     (1) Start Hotspot                       \n";
+	cout << "                     (2) Stop Hotspot                        \n";
+	cout << "               (3) Change Name and Password                  \n";
+	cout << "                                                             \n";
+	cout << "                                                             \n";
+	cout << "                                                             \n";
+	cout << "                                                             \n";
+	cout << "                                                             \n";
+	char menu = getch();
+	switch (menu)
+	{
+	case 1:
+		
+			
+			break;
+	case 2:
+	
+		break;
+	case 3:
+
+		break;
+	}
+}
