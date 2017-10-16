9241ba0323ca4ecad61c35244b242e3f886cf687
Ata Hakcil <ahakcil@gmail.com>
2017-10-17 01:03:00 +0300

    save

--- Image Manip/main.c
+++ Image Manip/main.c
@@ -13,15 +13,15 @@ int main(void)
 	FILE * ioftp;
 
 	int red, green, blue;
-	int i = 0;
-	char ch;
+	int i = 54;
+	char ch=0;
 
-	ioftp = fopen(filename, "ab+");
-
+	ioftp = fopen(filename, "rb+");
+	
 	red = 0;
 	green = 0;
 	blue = 0;
-	fseek(ioftp, 54, 0);
+	fseek(ioftp, 54, SEEK_SET);
 	while ((ch = getc(ioftp)) != EOF)
 	{
 		switch (i % 3)
@@ -36,24 +36,21 @@ int main(void)
 
 		case 2:
 			red = ch;
+			printf("%x %x %x\n", blue, green, red);
+			if (blue == 196 && green == 196 && red == 196)
+			{
+				printf("This is text");
+			}
+			else {
+				fseek(ioftp, i-3, SEEK_SET);
+				fprintf(ioftp, 0);
+				fprintf(ioftp, 0);
+				fprintf(ioftp, 0);
+				fseek(ioftp, i, SEEK_SET);
+			}
 			break;
-		}
-		i++;
+		}i++;
 	}
-	if (blue == 196 && green == 196 && red == 196)
-	{
-	}
-	else {
-		fseek(ioftp, -3, 0);
-		fprintf(ioftp, 0);
-		fprintf(ioftp, 0);
-		fprintf(ioftp, 0);
-	}
-
-
-
-
-
 	fclose(ioftp);
 	return 0;
 }
