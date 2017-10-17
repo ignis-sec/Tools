915685205da47b9f473bc4d877c947307d0dee4b
Ata Hakcil <ahakcil@gmail.com>
2017-10-17 03:44:45 +0300

    Better Definitions

--- Image Manip/main.c
+++ Image Manip/main.c
@@ -1,19 +1,36 @@
+//	Name: Isolate Text From 24 bit BMP
+
+//	Author: Ata Hakcil
+//	Description:Isolate text inside BMP image from the background
+
 #include <stdio.h>
 #include <stdlib.h>
 #define SHADE 196
-#define TOLERANCE 0
-//	Name: Isolate Text
+#define SHADE_RED SHADE								//if color is grayscale keep SHADERED/GREEN/BLUE defined as SHADE and change SHADE
+#define SHADE_BLUE SHADE
+#define SHADE_GREEN SHADE
 
-//	Author: Ata Hakcil
-//	Description:Isolate unicolor text from background
+#define TOLERANCE 0									//change below if you want custom tolerance on each color
+#define TOLERANCE_RED TOLERANCE
+#define TOLERANCE_BLUE TOLERANCE
+#define TOLERANCE_GREEN TOLERANCE
 
+#define COLOR_TEXT 0
+#define COLOR_TEXT_RED	COLOR_TEXT
+#define COLOR_TEXT_GREEN COLOR_TEXT
+#define COLOR_TEXT_BLUE COLOR_TEXT
+
+#define COLOR_BG 255
+#define COLOR_BG_RED	COLOR_BG
+#define COLOR_BG_GREEN COLOR_BG
+#define COLOR_BG_BLUE COLOR_BG
 
 //BMP architecture
-// 54 bytes of header
-//
-//
-//
-//
+// 54 bytes of header for 24 bit image 
+// BM tag image sizes colors definition etc
+// *****************VERY IMPORTANT NULL BYTES**********************
+//If a line of pixels is not size of 4n, file will be filled with 000 pixels until its size of 4n
+//BMP extension has no end file, it instead declares size at the start. Instead i declared 0,255,0 (green) as my EOF
 //
 //
 //
@@ -50,39 +67,41 @@ int main(void)
 
 	fseek(ioftp, 0, SEEK_CUR);
 	fseek(ioftp, 54, SEEK_SET);						//seek for 54th byte from start:: SEEK_SET->From Start
-	while ((ch = getc(ioftp)) != EOF)				//get char as long as it is	   :: SEEK_CUR->Current
-	{												//not end of file              :: SEEK_END->From End
+	while ((ch = getc(ioftp)) != EOF)				//get char as long as it is	   :: SEEK_CUR->Current			  Also EOF doesn't do anything 
+	{												//not end of file              :: SEEK_END->From End		  here, but might as well stay
 		switch (i % 3)								//switch on i%3 to check if it is r,g or b
 		{
-		case 0:										//case blue
+		case 0:										
 			blue = ch;
 			break;
 
-		case 1:										//case green
+		case 1:										
 			green = ch;
 			break;
 
-		case 2:										//case red; color cells in bmp end with red, so logic is applied here. 
+		case 2:										//Color cells in bmp end with red, so logic is applied here. 
 			red = ch;								//If last cell has our SHADE,SHADE,SHADE grey, then we ignore (it is a text pixel)
 			if (red == 0 && blue == 0 && green == 255) return 0;
-			if (((blue >= SHADE-TOLERANCE)&&(blue <= SHADE + TOLERANCE))&&((green >= SHADE - TOLERANCE)&&(green <= SHADE + TOLERANCE))&&((red >= SHADE - TOLERANCE)&&(red <= SHADE + TOLERANCE)))
+			if (((blue >= SHADE_BLUE-TOLERANCE_BLUE)&&(blue <= SHADE_BLUE + TOLERANCE_BLUE))			//if each color is in range of SHADE +- TOLERANCE
+				&&((green >= SHADE_GREEN - TOLERANCE_GREEN)&&(green <= SHADE_GREEN + TOLERANCE_GREEN))
+				&&((red >= SHADE_RED - TOLERANCE_RED)&&(red <= SHADE_RED + TOLERANCE_RED)))
 			{
 				
-				//printf("%d %d %d\n", blue, green, red);
+				//printf("%d %d %d\n", blue, green, red);							//print statement slows down the code 100x times
 				fseek(ioftp, -3, SEEK_CUR);				//so we seek 3 btyes behind
-				fprintf(ioftp, "%c", 0);				//overwrite them with 0,0,0 (black) 
-				fprintf(ioftp, "%c", 0);
-				fprintf(ioftp, "%c", 0);
+				fprintf(ioftp, "%c", COLOR_TEXT_BLUE);				//overwrite them with 0,0,0 (black) 
+				fprintf(ioftp, "%c", COLOR_TEXT_GREEN);
+				fprintf(ioftp, "%c", COLOR_TEXT_RED);
 				fseek(ioftp, 0, SEEK_CUR);				//doesn't work without SEEK_CUR to itself(fuck C)
 			}
 			else {									//if it is SHADE,SHADE,SHADE; it is part of the background, 
 				if (red == 0 && blue == 0 && green == 0) {}
 				else { nc = 0; }
-				//printf("This isnt text:%d %d %d\n", blue, green, red);
+				//printf("This isnt text:%d %d %d\n", blue, green, red);			//print statement slows down the code 100x times
 				fseek(ioftp, -3, SEEK_CUR);			//so we seek 3 btyes behind
-				fprintf(ioftp, "%c", 255);			//overwrite them with 255,255,255 (white) 
-				fprintf(ioftp, "%c", 255);
-				fprintf(ioftp, "%c", 255);
+				fprintf(ioftp, "%c", COLOR_BG_BLUE);			//overwrite them with 255,255,255 (white) 
+				fprintf(ioftp, "%c", COLOR_BG_GREEN);
+				fprintf(ioftp, "%c", COLOR_BG_RED);
 				fseek(ioftp, 0, SEEK_CUR);			//doesn't work without SEEK_CUR to itself(fuck C)
 			}
 			break;
