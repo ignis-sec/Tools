167cf2967f50a45f7fcd8a4d7fa2d215914e2301
Ata Hakcil <ahakcil@gmail.com>
2017-10-17 19:51:19 +0300

    Encode/Decode added

--- Asal/Main.cpp
+++ Asal/Main.cpp
@@ -1,26 +1,31 @@
 #include <iostream>
 #include <math.h>
 #include <fstream>
+#include "Main.h"
 using namespace std;
+
+void Encode(char  data[4], int b, std::ofstream &primefile);
+
 
 int main(void)
 {
-	int maxNum;
+	int maxNum;												//initialize variables
 	bool *BinString;
 	ofstream primefile;
 	int c = 0;
 	int i = 0;
-	int string[100000];
-	primefile.open("primes.txt");
+	primefile.open("primes.dat");							//open encode file
+		
+	char data[4];
 
-	//get max number
+															//get max number
 	cout << "Enter largest number to check:";
 	cin >> maxNum;
 
-	//malloc binary string
+															//malloc binary string
 	BinString = (bool*)malloc(maxNum);
 
-	//initialize
+															//initialize the binary string
 	BinString[0] = 0;
 	BinString[1] = 0;
 	for (int a = 2; a <= maxNum; a++)
@@ -29,27 +34,33 @@ int main(void)
 		
 	}
 
-	//loop and remove
+															//loop and remove logic
 	for (int b = 2; b < maxNum; b++)
 	{
 		if (!(b % 1000000)) cout << b << " = " << b / 1000000 << "x 10^6 " << '\n';
 		if (BinString[b] == 1)
 		{
 			c++;
-			string[i] = b;
-			i++;
-			//primefile << b << ", ";
+			
+			Encode(data, b, primefile);
 		}
 		for (int d = 2; d <= maxNum / b; d++)
 		{
 			BinString[b*d] = 0;
 		}
 	}
-
-	for (i=i; i >= 0; i--)
-	{
-		primefile << string[i] << ", ";
-
-	}
+	if (!(maxNum % 1000000)) cout << maxNum << " = " << maxNum / 1000000 << "x 10^6 " << '\n';		//report position
 	return 0;
+}
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+void Encode(char  data[4], int b, std::ofstream &primefile)
+{
+	data[0] = static_cast<char>(b & 0xFF);
+	data[1] = static_cast<char>((b >> 8) & 0xFF);
+	data[2] = static_cast<char>((b >> 16) & 0xFF);
+	data[3] = static_cast<char>((b >> 24) & 0xFF);
+	primefile.write(data, 4);
 }
---
+++ Read/Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(void)
+{
+	ifstream infile;
+	int i = 0;
+
+	while()
+
+	return 0;
+}
