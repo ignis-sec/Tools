7f45592bc962bf58d9b7d79821789e7dd40711cd
Ata Hakcil <ahakcil@gmail.com>
2017-10-17 20:12:13 +0300

    Decoder Bugfix

--- Read/Main.cpp
+++ Read/Main.cpp
@@ -5,10 +5,24 @@ using namespace std;
 
 int main(void)
 {
+	cout << "Reading encoded file..\n\n\n";
 	ifstream infile;
+	infile.open("primes.dat");
 	int i = 0;
-
-	while()
-
+	int decoded;
+	char c = 0;
+	char data[4];
+	while (infile.get(c))
+	{
+		data[i%4] = c;
+		i++;
+		if (i % 4 == 0 && i > 1)
+		{
+			decoded = data[0] + data[1] * 16 * 16 + data[2] * 16 * 16 * 16 * 16 + data[3] * 16 * 16 * 16 * 16 * 16 * 16;
+		}
+		cout << decoded << ", ";
+	}
+	infile.close();
+	system("PAUSE");
 	return 0;
 }
