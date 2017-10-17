18abfbbb3b5d277e319b21c5fca6378ddedf5640
Ata Hakcil <ahakcil@gmail.com>
2017-10-17 19:15:20 +0300

    Optimization trial

--- Asal/Main.cpp
+++ Asal/Main.cpp
@@ -9,13 +9,17 @@ int main(void)
 	bool *BinString;
 	ofstream primefile;
 	int c = 0;
-
+	int i = 0;
+	int string[100000];
 	primefile.open("primes.txt");
+
 	//get max number
 	cout << "Enter largest number to check:";
 	cin >> maxNum;
+
 	//malloc binary string
 	BinString = (bool*)malloc(maxNum);
+
 	//initialize
 	BinString[0] = 0;
 	BinString[1] = 0;
@@ -24,6 +28,7 @@ int main(void)
 		BinString[a] = 1;
 		
 	}
+
 	//loop and remove
 	for (int b = 2; b < maxNum; b++)
 	{
@@ -31,13 +36,20 @@ int main(void)
 		if (BinString[b] == 1)
 		{
 			c++;
-			
-			primefile << b << ", ";
+			string[i] = b;
+			i++;
+			//primefile << b << ", ";
 		}
 		for (int d = 2; d <= maxNum / b; d++)
 		{
 			BinString[b*d] = 0;
 		}
+	}
+
+	for (i=i; i >= 0; i--)
+	{
+		primefile << string[i] << ", ";
+
 	}
 	return 0;
 }
