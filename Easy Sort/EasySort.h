3080a0bc2d1f5def77f7af39868e77636f654ece
Ata Hakcil <ahakcil@gmail.com>
2017-10-24 22:14:46 +0300

    Added insertion and merge

--- Easy Sort/EasySort.h
+++ Easy Sort/EasySort.h
@@ -4,30 +4,34 @@
 
 
 
-
+////////////////////////////////////////////////////////
 template <class T_DataType>
-int Bubble_Sort(T_DataType **arr, int size)
+int Bubble_Sort(T_DataType arr[], int size)
 {
-	int iSwap=0;
+	int swapcount =0;
 	bool sorted = false;
-
-	for (int i = 0; i < size && !sorted; ++i)
+	while (sorted == false)
 	{
 		sorted = true;
-		if (arr[i] >arr[i + 1])
+		for (int i = 0; i < size-1; ++i)
 		{
-			sorted = false;
-			swap(arr[i], arr[i + 1]);
-			iSwap++;
-		}	
+
+			if (arr[i] >arr[i + 1])
+			{
+				sorted = false;
+				swap(arr[i], arr[i + 1]);
+				swapcount++;
+			}
+		}
 	}
-	return iSwap;
+	return swapcount;
 }
 
+////////////////////////////////////////////////////////
 template <class T_DataType>
-int Selection_Sort(T_DataType **arr, int n)
+int Selection_Sort(T_DataType arr[], int n)
 {
-	int iSwap = 0;
+	int swapcount = 0;
 	for (int i = 0; i < n - 1; ++i)
 	{
 		int min = i;
@@ -36,17 +40,108 @@ int Selection_Sort(T_DataType **arr, int n)
 			if (arr[j] < arr[min]) min = j;			
 		}
 		swap(arr[i], arr[min]);
-		iSwap++;
+		swapcount++;
 	}
-	return iSwap;
+	return swapcount;
 }
-/*
+
+////////////////////////////////////////////////////////
 template <class T_DataType>
-void swap(T_DataType a, T_DataType b)
+int Insertion_Sort(T_DataType arr[], int n)
 {
-	T_DataType temp;
-	temp  =    a;
-	a     =    b;
-	b     =    temp;
-	return;
-}*/
+	
+	int i, key, j, swapcount =0;
+	for (i = 1; i < n; i++)
+	{
+		key = arr[i];
+		j = i - 1;
+		while (j >= 0 && arr[j] > key)
+		{
+			arr[j + 1] = arr[j];
+			j = j - 1;
+			swapcount++;
+		}
+		arr[j + 1] = key;
+	}
+	return swapcount;
+}
+
+////////////////////////////////////////////////////////
+template <class T_DataType>
+void merge(T_DataType arr[], int left, int middle, int right)
+{
+	int i, j, k;
+	int n1 = middle - left + 1;
+	int n2 = right - middle;
+
+	/* create temp arrays */
+	T_DataType *ArrLeft;
+	T_DataType *ArrRight;
+	ArrLeft = (T_DataType*)malloc(n1 * sizeof(T_DataType));
+	ArrRight = (T_DataType*)malloc(n2 * sizeof(T_DataType));
+
+	/* Copy data to temp arrays*/
+	for (i = 0; i < n1; i++)
+		ArrLeft[i] = arr[left + i];
+	for (j = 0; j < n2; j++)
+		ArrRight[j] = arr[middle + 1 + j];
+
+	/* Merge the temp arrays back into arr[l..r]*/
+	i = 0; // Initial index of first subarray
+	j = 0; // Initial index of second subarray
+	k = left; // Initial index of merged subarray
+	while (i < n1 && j < n2)
+	{
+		if (ArrLeft[i] <= ArrRight[j])
+		{
+			arr[k] = ArrLeft[i];
+			i++;
+		}
+		else
+		{
+			arr[k] = ArrRight[j];
+			j++;
+		}
+		k++;
+	}
+
+	/* Copy the remaining elements of ArrLeft[], if there
+	are any */
+	while (i < n1)
+	{
+		arr[k] = ArrLeft[i];
+		i++;
+		k++;
+	}
+
+	/* Copy the remaining elements of ArrRight[], if there
+	are any */
+	while (j < n2)
+	{
+		arr[k] = ArrRight[j];
+		j++;
+		k++;
+	}
+	free(ArrLeft);
+	free(ArrRight);
+}
+////////////////////////////////////////////////////////
+
+//if you are calling this function, pass (arr, 0, size-1)
+template <class T_DataType>
+int Merge_Sort(T_DataType arr[], int left_index, int right_index)
+{
+	int swapcount = 0;
+	
+	if (left_index < right_index)
+	{
+		int m = left_index + (right_index - left_index) / 2;
+
+		// Sort first and second halves
+		Merge_Sort(arr, left_index, m);
+		Merge_Sort(arr, m + 1, right_index);
+
+		merge(arr, left_index, m, right_index);
+	}
+	return swapcount;
+}
--- Easy Sort/Main.cpp
+++ Easy Sort/Main.cpp
@@ -6,25 +6,28 @@ using namespace std;
 
 int main(void)
 {
-	int *arr;
-	arr = (int*)malloc(10 * sizeof(int));
-	arr[0] = 15;
-	arr[1] = 21;
-	arr[2] = 53;
-	arr[3] = 5;
-	arr[4] = 23;
-	arr[5] = 65;
-	arr[6] = 12;
-	arr[7] = 27;
-	arr[8] = 45;
-	arr[9] = 5;
-	arr[10] = 8;
+///////////////////////////////////////////////////////////////
+	int *arr;								//initialize array
+	int swaps;								//
+	arr = (int*)malloc(11 * sizeof(int));	//
+	arr[0] = 15;							//
+	arr[1] = 21;							//
+	arr[2] = 53;							//
+	arr[3] = 5;								//
+	arr[4] = 23;							//
+	arr[5] = 65;							//
+	arr[6] = 12;							//
+	arr[7] = 27;							//
+	arr[8] = 45;							//
+	arr[9] = 5;								//
+	arr[10] = 8;							//
+//////////////////////////////////////////////
 
-
-	Bubble_Sort(&arr, 9);
+	swaps=Merge_Sort(arr,0, 10);
 
 	for (int i = 0; i < 9; i++)
 		cout << arr[i] << '\n';
+	cout << "swapped " << swaps << " times\n";
 		
 
 	system("PAUSE");
