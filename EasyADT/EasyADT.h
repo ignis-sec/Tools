7fe30b5c958c9c919ca093c404073c463e8a2c42
Ata Hakcil <ahakcil@gmail.com>
2017-11-28 02:34:41 +0300

    Save

--- EasyADT/EasyADT.h
+++ EasyADT/EasyADT.h
@@ -27,13 +27,14 @@ And override the virtual T_NodeType* createNode(); to take node properties as in
 */
 
 /*Returns table:
-		linklist::append()
+		LinkList::append()
 			0:list was empty, added to front
 			1:added to tail
 
+		LinkList::CreateNode()
+			ptr to the new node(should be checked if null)
 
-
-
+		
 
 
 
@@ -65,46 +66,100 @@ public:
 	position previous;
 };
 
-//linklist class decleration
+//LinkList class decleration
 template <class T_NodeType>
-class linklist {
+class LinkList {
 public:
-	position GetHead(){return head;};
-	position GetTail(){return tail;};
-	unsigned int GetSize(){return size;};
-	bool isEmpty();
-	int append(position newNodePtr);		//append to the end of linklist"
-	virtual T_NodeType* CreateNode();		//create a new node YOU SHOULD OVERRIDE THIS
+	position get_head(){return head;};
+	position get_tail(){return tail;};
+	unsigned int get_size(){return size;};
+	bool is_empty();
+	virtual int append(position newNodePtr);			//append to the end of LinkList"
+	T_NodeType* LinkList<T_NodeType>::create_node();		//create a new node 
+	virtual void clear();
+	virtual void print();
 private:
-	position head;
-	position tail;
+	position head=NULL;
+	position tail=NULL;
 	unsigned int size = 0;
 
 };
 
+//derieved class
+template <class T_NodeType>
+class DoublyLinkedList : private LinkList<T_NodeType>
+{
+	virtual int append(position newNodePtr);			//append to the end of LinkList"
+	T_NodeType* LinkList<T_NodeType>::create_node();		//create a new node 
+	virtual void clear();
+	virtual void print();
+};
 
-bool linklist<class T_NodeType>::isEmpty() {
-	if (GetSize()) return false;
+bool LinkList<class T_NodeType>::is_empty() {
+	if (get_size()) return false;
 	else return true;
 }
 
-int linklist<class T_NodeType>::append(position newNodePtr) {
-	if (isEmpty())
+int LinkList<class T_NodeType>::append(position newNodePtr) {
+	if (is_empty())
 	{
 		head = newNodePtr;
 		tail = newNodePtr;
+		size++;
 		return 0;
 	}
 	tail->next = newNodePtr;
 	tail = newNodePtr;
+	size++;
 	return 1;
 	
 }
 
-
-T_NodeType* linklist<T_NodeType>::CreateNode()
+template <class T_NodeType>
+T_NodeType* LinkList<T_NodeType>::create_node()					//user should check if enough memory is available (if return is nullptr)
 {
 	T_NodeType *newNode;
 	newNode = (T_NodeType*)malloc(sizeof(T_NodeType));
 	return newNode;
+}
+
+void LinkList<class T_NodeType>::clear()
+{
+	position previous = head, cur = head->next;
+	while (cur != NULL)											//traverse the list and free previous element each step
+	{
+		free(previous);
+		previous = cur;
+		cur = cur->next;
+	}
+	free(previous);												//free last element		
+	head = tail = NULL;											//remove dangling pointers
+	return;
+}
+
+void LinkList<class T_NodeType>::print()
+{
+	unsigned int i = 1;
+	position cur = head;
+	while (cur != NULL)
+	{
+		std::cout << i << ". " /* << YOUR VARIABLE HERE << */ << std::endl;
+	}
+	return;
+}
+
+
+int DoublyLinkedList<class T_NodeType>::append(position newNodePtr) {
+	if (is_empty())
+	{
+		head = newNodePtr;
+		tail = newNodePtr;
+		size++;
+		return 0;
+	}
+	tail->next = newNodePtr;
+	tail = newNodePtr;
+	size++;
+	return 1;
+
 }
