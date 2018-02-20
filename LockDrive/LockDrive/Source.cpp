2e8c4645f375a370d10276de71b3d305be0dbb3c
Ata Hakçıl <ahakcil@gmail.com>
2018-02-20 18:37:02 +0300

    added

---
+++ LockDrive/LockDrive/Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <Windows.h>
+
+int main(int argc, char* argv[]) {
+	char drive;
+	std::string command = "manage-bde -lock ";
+	const char* c_command;
+	if(argc!=1)
+		drive = argv[1][0];
+	else 
+	{
+		std::cout << "Enter drive letter:";
+		std::cin >> drive;
+	}
+	command.push_back(drive);
+	command.push_back(':');
+	c_command = command.c_str();
+	system(c_command);
+	system("PAUSE");
+	return 0;
+}
